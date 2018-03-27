2e4737d6a7bf6722329be60cbffb1d980ba006ea
sergey.cherkesov <sergey.cherkasov@devim.team>
2018-03-27 18:18:54 +0300

    Update types

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ API client realization
 ### Useful links
 
 * [Kassa API docs](https://kassa.yandex.ru/docs/checkout-api/#sozdanie-platezha)
+* [Payment lifecycle](https://kassa.yandex.ru/docs/guides/#platezhi)
--- exchanges.go
+++ exchanges.go
@@ -18,6 +18,14 @@ type Card struct {
 	CardType    string `json:"card_type"`
 }
 
+type PaymentMethod struct {
+	Type  string `json:"type"`
+	Id    string `json:"id"`
+	Saved bool   `json:"saved"`
+	Card  Card   `json:"card"`
+	Title string `json:"title"`
+}
+
 type CreatePaymentRequest struct {
 	Amount            Amount `json:"amount"`
 	PaymentMethodData struct {
@@ -30,17 +38,41 @@ type CreatePaymentRequest struct {
 }
 
 type CreatePaymentResponse struct {
-	Id            string      `json:"id"`
-	Status        string      `json:"status"`
-	Paid          bool        `json:"paid"`
-	Amount        Amount      `json:"amount"`
-	CreatedAt     time.Time   `json:"created_at"`
-	Metadata      interface{} `json:"metadata"`
-	PaymentMethod struct {
-		Type  string `json:"type"`
-		Id    string `json:"id"`
-		Saved bool   `json:"saved"`
-		Card  Card   `json:"card"`
-		Title string `json:"title"`
-	} `json:"payment_method"`
+	Id            string        `json:"id"`
+	Status        string        `json:"status"`
+	Paid          bool          `json:"paid"`
+	Amount        Amount        `json:"amount"`
+	CreatedAt     time.Time     `json:"created_at"`
+	Metadata      interface{}   `json:"metadata"`
+	PaymentMethod PaymentMethod `json:"payment_method"`
 }
+
+const (
+	PAYMENT_STATUS__WAIT      = "waiting_for_capture"
+	PAYMENT_STATUS__PENDING   = "pending"
+	PAYMENT_STATUS__SUCCEEDED = "succeeded"
+	PAYMENT_STATUS__CANCELED  = "canceled"
+)
+
+type PaymentResponse struct {
+	Id            string        `json:"id"`
+	Status        string        `json:"status"`
+	Paid          bool          `json:"paid"`
+	Amount        Amount        `json:"amount"`
+	Description   *string       `json:"description"`
+	CreatedAt     time.Time     `json:"created_at"`
+	Metadata      interface{}   `json:"metadata"`
+	PaymentMethod PaymentMethod `json:"payment_method"`
+}
+
+type GetPaymentResponse PaymentResponse
+
+type CapturePaymentRequest struct {
+	Amount Amount `json:"amount"`
+}
+
+type CapturePaymentResponse PaymentResponse
+
+type CancelPaymentRequest struct{}
+
+type CancelPaymentResponse PaymentResponse
